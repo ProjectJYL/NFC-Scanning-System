e6e51f29a02345665f9f2a9ea4f2e45bab1d9a22
ProjectJYL <jianyet.lee@gmail.com>
2016-09-14 14:48:32 -0400

    Add files via upload

---
+++ Arduino/hekxo06102015/constants.h
@@ -0,0 +1,52 @@
+#include <SoftwareSerial.h>
+#include "Goldelox_Serial_4DLib.h"
+#include "Goldelox_Const4D.h"
+
+//from ble
+#define PLAY_CORRECT_LED 7
+#define PLAY_WRONG_LED 6
+#define NFC_READ_LED 5
+
+#define ADAFRUITBLE_REQ 10
+#define ADAFRUITBLE_RDY 3
+#define ADAFRUITBLE_RST 9
+
+#define UID_LENGTH_4 4
+#define UID_LENGTH_7 7
+
+uint8_t nfc_cmd[20];
+uint8_t dev_name[7];
+
+static Adafruit_BLE_UART uart(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
+//from nfc
+#define IRQ   (2)
+#define RESET (9)
+
+uint8_t uid_buffer[UID_LENGTH_4];
+uint8_t nfc_response;
+uint8_t global_tag_info[16];
+
+static Adafruit_NFCShield_I2C nfc(IRQ, RESET);
+
+//from image
+//for arduino uno 
+//SoftwareSerial DisplaySerial(12,13) ; // pin 12 = TX of display, pin 13 = RX
+#define DisplaySerial Serial1
+#define RESETLINE 4
+
+static Goldelox_Serial_4DLib Display(&DisplaySerial);
+
+word hndl;
+
+#define iUserimages1 0x0000 //start up animation 
+#define iUserimages2H 0x0004 //object image
+#define iUserimages2L 0x0200 //object image
+#define iUserimages3H 0x017B //great job animation
+#define iUserimages3L 0x8400 //great job animation
+#define iUserimages4H 0x01B9 //no image found
+#define iUserimages4L 0x8800
+
+#define iVideo1H 0x017E //clock animation
+#define iVideo1L 0x0600
+
+
---
+++ Arduino/hekxo06102015/image.ino
@@ -0,0 +1,80 @@
+/*************************************************************
+ * //@filename image.ino
+ * //@author Jian Yet Lee
+ * //Copyright (c) 2015 WebTeam Corporation. All rights reserved.
+ *************************************************************/
+
+void tft_loop() {
+  delay(2000);
+}
+
+void reset_display() {
+  digitalWrite(RESETLINE, HIGH);
+  delay(100);
+  digitalWrite(RESETLINE, LOW);
+}
+
+void mycallback(int errorCode, unsigned char errorByte) {
+  const char *Error4DText[] = {"OK\0", "Timeout\0", "NAK\0", "Length\0", "Invalid\0"};
+  Serial.print(F("Serial 4D Library reports error ")) ;
+  Serial.print(Error4DText[errorCode]);
+  if (errorCode == Err4D_NAK)
+  {
+    Serial.print(F(" returned data= "));
+    Serial.println(errorByte);
+  }
+  else {
+    Serial.println(F(""));
+  }
+}
+
+/*
+uint8_t transfer_img(char * filename) {
+  //open a new file for write only
+  hndl = Display.file_Open(filename, 'w');
+  if (Display.file_Error() != 0)
+  {
+    Serial.println("File error");
+    return Display.file_Error();
+  }
+  //if the streaming is not finished keep copying ble buffer
+  while (nfc_cmd[0] != 0x39 && strlen((char *)nfc_cmd) != 1) {
+    //loop  until all bytes are transferred
+    if (uart.available())
+    {
+      //read next byte from buffer
+      Display.file_PutC(uart.read(), hndl);
+    }
+  }
+  //wait for cmd 0x39 and then exits the loop
+  //release the file handle
+  Display.file_Close(hndl);
+  return 0x39;
+}
+
+void displayReadByte(char * data, word &handle) {
+  Serial.print(F("return: ")); Serial.println(Display.file_Read(data , 10, handle));
+  if (Display.file_Error() > 0)
+  {
+    Serial.print(F("File Error after reading: ")); Serial.println(Display.file_Error());
+    //    hndl = Display.file_Open("image8.txt", 'r');
+  }
+}
+
+void mount_sd() {
+  disk = Display.file_Mount();
+  if (!disk) {
+    Display.putstr("Mount uSD card failed\n");
+    Display.putstr("Please insert uSD card\n");
+    while (!disk) {
+      disk = Display.file_Mount();
+    }
+    Display.gfx_Cls();
+    Display.putstr("File mounted\n");
+    delay(1000);
+  } else {
+    Display.putstr("File mounted\n");
+    delay(1000);
+  }
+}
+*/
